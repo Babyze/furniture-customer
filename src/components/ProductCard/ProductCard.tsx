86674a384a2c0b6f44c994b103f34271d3daf3d4
Babyze <danghuynhanh1999@gmail.com>
2025-03-31 08:30:53 +0700

    Update CSS

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,7 +1,5 @@
-import { useCart } from '@src/hooks/useCart';
 import { Link } from 'react-router';
 import './ProductCard.css';
-import { Button } from '../ui/Button';
 
 interface ProductCardProps {
   id: string;
@@ -12,40 +10,22 @@ interface ProductCardProps {
 }
 
 export const ProductCard = ({ id, name, minPrice, maxPrice, image }: ProductCardProps) => {
-  const { addItem } = useCart();
-
-  const handleAddToCart = () => {
-    addItem({
-      id,
-      name,
-      minPrice,
-      maxPrice,
-      image,
-    });
-  };
-
   return (
-    <div className="product-card">
+    <Link to={`/products/${id}`} className="product-card">
       <div className="product-card__image-container">
         <img
           src={image ? `http://${image}` : '/public/unknown-image-product.png'}
           alt={name}
           className="product-card__image"
         />
-
-        <Button variant="primary" className="product-card__add-to-cart" onClick={handleAddToCart}>
-          Add to Cart
-        </Button>
       </div>
       <div className="product-card__content">
-        <Link to={`/products/${id}`} className="product-card__name">
-          {name}
-        </Link>
+        <h3 className="product-card__name">{name}</h3>
         <div className="product-card__prices">
           <span className="product-card__price">${minPrice.toFixed(2)}</span>
           <span className="product-card__price">${maxPrice.toFixed(2)}</span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
